74e343515877e94d6f164ed62831d47c608dcf62
Shlok Sane <shloksane11@gmail.com>
2025-11-10 04:45:47 +0000

    You haven't done it, I want you to make it bit bigger. It is looking ver

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -48,8 +48,8 @@ export const Icons = {
   heartQuestion: (props: LucideProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
+      width="36"
+      height="36"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -61,7 +61,7 @@ export const Icons = {
       <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
       <text
         x="12"
-        y="11"
+        y="12"
         textAnchor="middle"
         alignmentBaseline="middle"
         fontSize="9"
