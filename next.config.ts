2e35310dacd2bc2799a87f655c43c26813a17574
shlok11 <shloksane11@gmail.com>
2025-11-12 10:58:05 +0530

    images done 2

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
